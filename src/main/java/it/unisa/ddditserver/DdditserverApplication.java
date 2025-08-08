4ebeaf8e31db7d62c235de33e4a4dbf4668e6519
angeloantonioprisco <a.prisco50@studenti.unisa.it>
2025-08-08 12:52:58 +0200

    First commit.

---
+++ src/main/java/it/unisa/ddditserver/DdditserverApplication.java
@@ -0,0 +1,13 @@
+package it.unisa.ddditserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdditserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdditserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/unisa/ddditserver/DdditserverApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unisa.ddditserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdditserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
