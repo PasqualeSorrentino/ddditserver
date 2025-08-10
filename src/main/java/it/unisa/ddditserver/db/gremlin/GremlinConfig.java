5a46084bb43141f6501ec9944aa1cd5c0adf20b7
angeloantonioprisco <a.prisco50@studenti.unisa.it>
2025-08-10 18:44:25 +0200

    Added GremlinConfig.java as a class to manage the configuration of Gremlin database through Azure.

---
+++ src/main/java/it/unisa/ddditserver/db/gremlin/GremlinConfig.java
@@ -0,0 +1,19 @@
+package it.unisa.ddditserver.db.gremlin;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Component
+public class GremlinConfig {
+
+    @Value("${GREMLIN_ENDPOINT}")
+    private String endpoint;
+
+    @Value("${GREMLIN_USERNAME}")
+    private String username;
+
+    @Value("${GREMLIN_KEY}")
+    private String key;
+}
