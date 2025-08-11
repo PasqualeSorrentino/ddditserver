4845a68fc611fdb34f04f1c45fc9895937d17b6d
angeloantonioprisco <a.prisco50@studenti.unisa.it>
2025-08-11 10:54:30 +0200

    Deleted GremlinAuthInterface.java, UserValidator.java and Validator.java.

--- src/main/java/it/unisa/ddditserver/validators/Validator.java
+++
@@ -1,7 +0,0 @@
-package it.unisa.ddditserver.validators;
-
-import it.unisa.ddditserver.validators.implementations.ValidationResult;
-
-public interface Validator<T> {
-    ValidationResult validate(T input);
-}
