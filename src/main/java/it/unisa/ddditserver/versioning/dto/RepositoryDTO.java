398fde621d1078de427e3c8453cb93a9b5b49778
angeloantonioprisco <a.prisco50@studenti.unisa.it>
2025-08-12 12:37:29 +0200

    Added RepositoryDTO.java to map repository's information.

---
+++ src/main/java/it/unisa/ddditserver/versioning/dto/RepositoryDTO.java
@@ -0,0 +1,12 @@
+package it.unisa.ddditserver.versioning.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RepositoryDTO {
+    private String repositoryName;
+}
