c4a40f309ad0bc6123dc6752a8711234b2f6319a
angeloantonioprisco <a.prisco50@studenti.unisa.it>
2025-08-15 14:20:40 +0200

    Added InvitationDTO.java to map invitation's information.

---
+++ src/main/java/it/unisa/ddditserver/subsystems/invitation/dto/InvitationDTO.java
@@ -0,0 +1,21 @@
+package it.unisa.ddditserver.subsystems.invitation.dto;
+
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+/**
+ * Data Transfer Object (DTO) representing an invitation to a repository.
+ *
+ * <ul>
+ *     <li>{@code username} - the username of the user being invited.</li>
+ *     <li>{@code repositoryName} - the name of the repository to which the user is invited.</li>
+ * </ul>
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class InvitationDTO {
+    String username;
+    String repositoryName;
+}
