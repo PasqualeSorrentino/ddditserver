bae360510812e51b61401bbc123923a5d58e12d3
angeloantonioprisco <a.prisco50@studenti.unisa.it>
2025-08-16 14:39:51 +0200

    Added comments to CosmosAuthServiceImpl.java and removed unnecessary exceptions.

--- src/main/java/it/unisa/ddditserver/db/cosmos/auth/CosmosAuthServiceImpl.java
+++ src/main/java/it/unisa/ddditserver/db/cosmos/auth/CosmosAuthServiceImpl.java
@@ -3,26 +3,28 @@ package it.unisa.ddditserver.db.cosmos.auth;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.azure.cosmos.*;
-import it.unisa.ddditserver.auth.dto.BlacklistedTokenDTO;
-import it.unisa.ddditserver.auth.exceptions.AuthException;
+import com.azure.cosmos.models.PartitionKey;
+import it.unisa.ddditserver.subsystems.auth.dto.BlacklistedTokenDTO;
+import it.unisa.ddditserver.subsystems.auth.exceptions.AuthException;
 import it.unisa.ddditserver.db.cosmos.CosmosConfig;
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CosmosAuthServiceImpl implements CosmosAuthService {
     private final CosmosConfig config;
-    private CosmosClient cosmosClient;
     private CosmosContainer blacklistContainer;
 
+    @Autowired
     public CosmosAuthServiceImpl(CosmosConfig config) {
         this.config = config;
     }
 
     @PostConstruct
     public void init() {
-        // Initialize Cosmos DB client
-        this.cosmosClient = new CosmosClientBuilder()
+        // Build client connection to Cosmos server
+        CosmosClient cosmosClient = new CosmosClientBuilder()
                 .endpoint(config.getEndpoint())
                 .key(config.getKey())
                 .buildClient();
@@ -35,36 +37,36 @@ public class CosmosAuthServiceImpl implements CosmosAuthService {
     }
 
     @Override
-    public void blacklistToken(String tokenId) {
+    public void blacklistToken(String token) {
         try {
-            DecodedJWT decodedJWT = JWT.decode(tokenId);
+            DecodedJWT decodedJWT = JWT.decode(token);
             long tokenExpiryTimestamp = decodedJWT.getExpiresAt().getTime() / 1000;
             long currentTimestamp = System.currentTimeMillis() / 1000;
 
             int remainingTtl = (int) (tokenExpiryTimestamp - currentTimestamp);
 
-            if (remainingTtl <= 0) {
-                throw new AuthException("Token already expired, no need to blacklist");
-            }
+            if (remainingTtl <= 0) return;
 
-            BlacklistedTokenDTO blacklistedToken = new BlacklistedTokenDTO(tokenId, tokenId, remainingTtl);
-
+            BlacklistedTokenDTO blacklistedToken = new BlacklistedTokenDTO(token, token, remainingTtl);
             blacklistContainer.upsertItem(blacklistedToken);
-
         } catch (Exception e) {
+            // If it is necessary use a RuntimeException for more detailed debug
             throw new AuthException("Error blacklisting token");
         }
     }
 
     @Override
-    public boolean isTokenBlacklisted(String tokenId) {
+    public boolean isTokenBlacklisted(String token) {
         try {
-            BlacklistedTokenDTO token = blacklistContainer.readItem(tokenId, new com.azure.cosmos.models.PartitionKey(tokenId), BlacklistedTokenDTO.class).getItem();
-            return token != null;
+            BlacklistedTokenDTO blacklistedTokenDTO = blacklistContainer.
+                    readItem(token, new PartitionKey(token), BlacklistedTokenDTO.class).getItem();
+
+            return blacklistedTokenDTO!= null;
         } catch (CosmosException e) {
             if (e.getStatusCode() == 404) {
                 return false;
             }
+            // If it is necessary use a RuntimeException for more detailed debug
             throw new AuthException("Error checking blacklisted token");
         }
     }
