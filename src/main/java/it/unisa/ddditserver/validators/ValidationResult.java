3d6d8626488b20ec12dd790dc2ac23cd11fa4816
angeloantonioprisco <a.prisco50@studenti.unisa.it>
2025-08-25 18:26:29 +0200

    Modified ValidationResult.java to manage exceptions instead of messages.

--- src/main/java/it/unisa/ddditserver/validators/ValidationResult.java
+++ src/main/java/it/unisa/ddditserver/validators/ValidationResult.java
@@ -5,20 +5,33 @@ import lombok.Value;
 /**
  * Represents the result of a validation check.
  *
+ * It includes an optional exception if the validation fails.
+ * The exception includes details about the validation failure.
+ *
  * @author Angelo Antonio Prisco
- * @version 1.0
- * @since 2025-08-11
+ * @version 1.1
+ * @since 2025-08-14
  */
 @Value
 public class ValidationResult {
     boolean valid;
-    String message;
+    Exception exception;
 
-    public static ValidationResult valid() {
-        return new ValidationResult(true, null);
+    public ValidationResult(boolean valid) {
+        this.valid = valid;
+        this.exception = null;
     }
 
-    public static ValidationResult invalid(String message) {
-        return new ValidationResult(false, message);
+    public ValidationResult(boolean valid, Exception exception) {
+        this.valid = valid;
+        this.exception = exception;
+    }
+
+    public static ValidationResult valid() {
+        return new ValidationResult(true);
+    }
+
+    public static ValidationResult invalid(Exception exception) {
+        return new ValidationResult(false, exception);
     }
 }
