423384a8e42b7ce6ae7b4af6dc779cf14af5d292
PasqualeSorrentino <119622217+PasqualeSorrentino@users.noreply.github.com>
2025-11-07 10:54:18 +0100

    Modifiche testing

--- src/test/java/it/unisa/ddditserver/DdditserverApplicationTests.java
+++ src/test/java/it/unisa/ddditserver/DdditserverApplicationTests.java
@@ -3,8 +3,7 @@ package it.unisa.ddditserver;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
-
+@SpringBootTest
 class DdditserverApplicationTests {
 
 	@Test
