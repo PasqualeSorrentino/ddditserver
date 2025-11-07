1f130f04360dec83f7250a8d5c10ba9326ec6240
PasqualeSorrentino <119622217+PasqualeSorrentino@users.noreply.github.com>
2025-11-07 12:23:26 +0100

    Test Modification

--- src/test/java/it/unisa/ddditserver/db/integration/GremlinVersionRepositoryImplTest.java
+++ src/test/java/it/unisa/ddditserver/db/integration/GremlinVersionRepositoryImplTest.java
@@ -36,20 +36,20 @@ class GremlinVersionRepositoryImplTest {
 
     // ATTENTION: currently, none of these environment variables exist on GitHub or on the VM,
     // mocking configs is necessary to ensure repository operations in the test environment are separated from production
-    @BeforeAll
-    void setUp() {
-        when(gremlinConfig.getEndpoint()).thenReturn(System.getenv("TEST_GREMLIN_ENDPOINT"));
-        when(gremlinConfig.getUsername()).thenReturn(System.getenv("TEST_GREMLIN_USERNAME"));
-        when(gremlinConfig.getKey()).thenReturn(System.getenv("TEST_GREMLIN_KEY"));
-
-        when(cosmosConfig.getEndpoint()).thenReturn(System.getenv("TEST_COSMOS_SQL_ENDPOINT"));
-        when(cosmosConfig.getKey()).thenReturn(System.getenv("TEST_COSMOS_SQL_KEY"));
-        when(cosmosConfig.getDatabaseName()).thenReturn(System.getenv("TEST_COSMOS_SQL_DATABASE"));
-        when(cosmosConfig.getVersionsContainerName()).thenReturn(System.getenv("TEST_COSMOS_SQL_CONTAINER_VERSIONS"));
-        when(cosmosConfig.getTokenBlacklistContainerName()).thenReturn(System.getenv("TEST_COSMOS_SQL_CONTAINER_TOKEN_BLACKLIST"));
-
-        when(blobStorageConfig.getConnectionString()).thenReturn(System.getenv("TEST_BLOB_STORAGE_CONNECTION_STRING"));
-        when(blobStorageConfig.getConnectionString()).thenReturn(System.getenv("TEST_BLOB_STORAGE_CONTAINER_MESHES"));
-        when(blobStorageConfig.getConnectionString()).thenReturn(System.getenv("BLOB_STORAGE_CONTAINER_MATERIALS"));
-    }
+    //@BeforeAll
+    //void setUp() {
+    //    when(gremlinConfig.getEndpoint()).thenReturn(System.getenv("TEST_GREMLIN_ENDPOINT"));
+    //    when(gremlinConfig.getUsername()).thenReturn(System.getenv("TEST_GREMLIN_USERNAME"));
+    //    when(gremlinConfig.getKey()).thenReturn(System.getenv("TEST_GREMLIN_KEY"));
+//
+    //    when(cosmosConfig.getEndpoint()).thenReturn(System.getenv("TEST_COSMOS_SQL_ENDPOINT"));
+    //    when(cosmosConfig.getKey()).thenReturn(System.getenv("TEST_COSMOS_SQL_KEY"));
+    //    when(cosmosConfig.getDatabaseName()).thenReturn(System.getenv("TEST_COSMOS_SQL_DATABASE"));
+    //    when(cosmosConfig.getVersionsContainerName()).thenReturn(System.getenv("TEST_COSMOS_SQL_CONTAINER_VERSIONS"));
+    //    when(cosmosConfig.getTokenBlacklistContainerName()).thenReturn(System.getenv("TEST_COSMOS_SQL_CONTAINER_TOKEN_BLACKLIST"));
+//
+    //    when(blobStorageConfig.getConnectionString()).thenReturn(System.getenv("TEST_BLOB_STORAGE_CONNECTION_STRING"));
+    //    when(blobStorageConfig.getConnectionString()).thenReturn(System.getenv("TEST_BLOB_STORAGE_CONTAINER_MESHES"));
+    //    when(blobStorageConfig.getConnectionString()).thenReturn(System.getenv("BLOB_STORAGE_CONTAINER_MATERIALS"));
+    //}
 }
